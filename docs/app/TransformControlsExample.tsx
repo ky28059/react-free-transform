dc24bde399519a9b59fab00198530add9f61cb33
Kevin Yu <kevinyu567@gmail.com>
2025-06-13 12:10:47 -0700

    Update docs source for new API

--- docs/app/TransformControlsExample.tsx
+++ docs/app/TransformControlsExample.tsx
@@ -50,13 +50,13 @@ export default function TransformControlsExample() {
 const code = `import { useFreeTransform } from '@ky28059/react-free-transform';
 
 export default function TransformControlsExample() {
-    const { transform, zoom, reset, callbacks } = useFreeTransform();
+    const { register, transform, zoom, reset } = useFreeTransform();
 
     return (
         <div className="relative mt-5 mb-8">
             <div
                 className="flex items-center justify-center overflow-hidden touch-none rounded border border-black/10 bg-pink-100 bg-[linear-gradient(135deg,#fb64b680_10%,#0000_0,#0000_50%,#fb64b680_0,#fb64b680_60%,#0000_0,#0000)] bg-[length:7.07px_7.07px] h-96"
-                {...callbacks}
+                ref={register}
             >
                 <img
                     src="/2.jpg"
